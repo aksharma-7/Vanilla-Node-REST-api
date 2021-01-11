b9ed823065314fdc6a2ee91adde69225780f3401
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-11 16:40:19 +0530

    getting product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require('../models/productModel');
 
+// gets all Products
+//  GET /api/products
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll()
@@ -10,6 +12,26 @@ async function getProducts(req, res) {
     }
 }
 
+// gets single Product
+//  GET /api/product/:id
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id)
+
+        if(!product) {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({message: 'Product not found'}));
+        } else {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(product));
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 }

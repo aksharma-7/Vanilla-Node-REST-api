aed9dbaa2d9139ca03c670a6eb0047cad52aff1d
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-11 16:09:11 +0530

    Initalised the project an created the server

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const products = require('./data/products');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(products));
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
